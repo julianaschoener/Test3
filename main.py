d2d2e097095a59de385616d45115c3a7029beb31
juliana schoener <juli.schoener@gmail.com>
2024-05-07 15:19:36 +0200

    TEST 3 SCHÔNER

--- main.py
+++ main.py
@@ -2,6 +2,31 @@
 Upper and Lower
 """
 # Provide your solution here
+def count_upper_lower(my_string: str):
+    my_string =
+        my_string_upper = len(my_string.isupper())
+        len(my_string.isupper()))
+    print(my_string)
+    #number_uppercase =
+
+count_upper_lower("Hello")
+
+#def super_print(text: str,uppercase: bool):
+def count_upper_lower(my_string: str)
+
+    my_string = input("Please type in a string:")
+
+    for i in range:
+        upper_no = len(my_string.isupper())
+        print("Number of upper case characters: " + upper_no)
+
+    for i in range:
+        lower_no = len(my_string.islower())
+    print("Number of lower case characters: " + lower_no)
+
+count_upper_lower()
+
+
 
 """
 Check 33
@@ -9,6 +34,21 @@ Check 33
 # Provide your solution here
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
 
+def has_33(my_list: int) -> bool:
+        my_list = [1, 3, 3,]
+        my_list =   my_list.sort()
+
+        if my_list [1] = my_list[2]
+            return True
+
+        else:
+            return False
+
+if __name__ == '__main__': [1,3,3]
+if __name__ == '__main__': [1,4,4]
+if __name__ == '__main__': [3,1,3]
+
+has33()
